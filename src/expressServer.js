aef10c92b8c10a3cd96987f8885dd03300d717e8
kharelutsav <kharelutsav4@gmail.com>
2022-03-15 20:17:27 +0545

    Static files serving for logo servings

--- src/expressServer.js
+++ src/expressServer.js
@@ -8,6 +8,7 @@ app.set("view engine", 'pug');
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('static'));
 
 app.use('/', router);
 
